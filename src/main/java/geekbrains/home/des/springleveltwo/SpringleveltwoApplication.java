50d8b103e63a85d7630f47e9f4d1cae8b431d71c
Leonid <kissmykrabeskill@gmail.com>
2020-10-06 16:01:11 +0500

    Initial Project
    project init

---
+++ src/main/java/geekbrains/home/des/springleveltwo/SpringleveltwoApplication.java
@@ -0,0 +1,13 @@
+package geekbrains.home.des.springleveltwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringleveltwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringleveltwoApplication.class, args);
+	}
+
+}
