c0ce772785f8162c6dd5ee8ecea5c6a64de0cc59
Leonid <kissmykrabeskill@gmail.com>
2020-10-20 14:23:22 +0500

    Add AOP

---
+++ aop/src/main/java/aspect/LogAspect.java
@@ -0,0 +1,40 @@
+package aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+public class LogAspect {
+
+    @Pointcut("")
+    private void anyMethod(){
+    }
+
+    @Before("anyMethod()")
+    public void logBefore(JoinPoint joinPoint){
+        System.out.println("log before anyMethod: " + joinPoint);
+    }
+
+    @After("anyMethod()")
+    public void logAfter(JoinPoint joinPoint){
+        System.out.println("log after anyMethod:" + joinPoint);
+    }
+
+    @AfterReturning("anyMethod()")
+    public void logAfterReturning(JoinPoint joinPoint){
+        System.out.println("log after returning anyMethod" + joinPoint);
+    }
+
+    @AfterThrowing("anyMethod()")
+    public void logAfterThrowing(JoinPoint joinPoint){
+        System.out.println("log after throwing anyMethod" + joinPoint);
+    }
+
+    @Around("anyMethod()")
+    private Object logAround(ProceedingJoinPoint pj) throws Throwable {
+        System.out.println("log around after" + System.currentTimeMillis());
+        Object value = pj.proceed();
+        System.out.println("log around beforer" + System.currentTimeMillis());
+        return value;
+    }
+}
